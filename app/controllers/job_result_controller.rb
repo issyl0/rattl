bf8301874eff2bd1277c2f6cd07fc35254a179d9
issyl0 <isabell@issyl0.co.uk>
2014-03-14 13:19:17 +0000

    Half of the comparison/awarding process is done
    - Need to do thesaurus checking and comparison of skills, and give the
      user some feedback.

--- app/controllers/job_result_controller.rb
+++ app/controllers/job_result_controller.rb
@@ -1,6 +1,26 @@
 class JobResultController < ApplicationController
   def verdict
-    # Redirect to root path for now.
-    redirect_to root_path
+    # Get the SOC code for the particular job.
+    soc_result = JSON.parse(RestClient.get("http://api.lmiforall.org.uk/api/v1/soc/search?q=#{$job_name.gsub(" ", "+")}"))
+    
+    if soc_result[0]
+      # Convert the UK SOC code to its US ONET code.
+      onet_result = JSON.parse(RestClient.get("http://api.lmiforall.org.uk/api/v1/o-net/soc2onet/#{soc_result[0]['soc']}"))['onetCodes'][0]['code']
+    
+      # Search the skills endpoint for the ONET code.
+      preprocesed_onet_skills = JSON.parse(RestClient.get("http://api.lmiforall.org.uk/api/v1/o-net/skills/#{onet_result}"))['scales'][0]['skills']
+      # Push the ONET skills data into a hash for later processing.
+      onet_skills = Hash.new
+      preprocesed_onet_skills.each do |os|
+        onet_skills[:name] = os['name']
+        onet_skills[:value] = os['value']
+      end
+
+      # Compare the user's skills to the specified skills, with the
+      # help of the thesaurus API.
+      # If the user has > 5 skills, award him/her the job.
+    else
+      redirect_to job_search_path # Need to add an error message.
+    end
   end
 end
