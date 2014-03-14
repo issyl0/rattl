aef7d967d4fb22fad85ab1667250f9ccd96ccf7d
issyl0 <isabell@issyl0.co.uk>
2014-03-14 14:28:15 +0000

    Check against the LMI API job abilities, not just skills

--- app/controllers/job_result_controller.rb
+++ app/controllers/job_result_controller.rb
@@ -15,10 +15,16 @@ class JobResultController < ApplicationController
         onet_skills[:name] = os['name']
         onet_skills[:value] = os['value']
       end
+      # Search the abilities endpoint for the ONET code.
+      preprocessed_onet_abilities = JSON.parse(RestClient.get("http://api.lmiforall.org.uk/api/v1/o-net/abilities/#{onet_result}"))['scales'][0]['abilities']
+      # Push the ONET abilities data into a hash for later processing.
+      onet_abilities = Hash.new
+      preprocessed_onet_abilities.each do |oa|
+        onet_abilities[:name] = oa['name']
+        onet_abilities[:value] = oa['value']
+      end
 
-      # Compare the user's skills to the specified skills, with the
-      # help of the thesaurus API.
-      # If the user has > 5 skills, award him/her the job.
+      # If the user has > 5 skills matched, award him or her the job.
     else
       redirect_to job_search_path # Need to add an error message.
     end
